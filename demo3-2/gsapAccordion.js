7811d39c28cec1d57a5744c3b04fe4ed49cf5409
ics-sawada-h <haruka.sawada@ics-web.jp>
2022-09-06 09:43:45 +0900

    文言修正等

--- demo3-2/gsapAccordion.js
+++ demo3-2/gsapAccordion.js
@@ -16,18 +16,18 @@ const setUpAccordion = () => {
       // デフォルトの挙動を無効化
       event.preventDefault();
 
-      // is-openクラスの有無で判定
-      if (element.classList.contains("is-open")) {
+      // is-openedクラスの有無で判定
+      if (element.classList.contains("is-opened")) {
         // アコーディオンを閉じるときの処理
-        // アイコンを操作するためのis-openクラスを切り替える(クラスを取り除く)
-        element.classList.toggle("is-open");
+        // アイコンを操作するためのis-openedクラスを切り替える(クラスを取り除く)
+        element.classList.toggle("is-opened");
 
         // アニメーション実行
         closeAccordion(content, element).restart();
       } else {
         // アコーディオンを開くときの処理
-        // アイコンを操作するためのis-openクラスを切り替える(クラスを付与)
-        element.classList.toggle("is-open");
+        // アイコンを操作するためのis-openedクラスを切り替える(クラスを付与)
+        element.classList.toggle("is-opened");
 
         // open属性を付与
         element.setAttribute("open", "true");
